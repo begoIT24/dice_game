3439c7cc1f5092326c507cee72e622649d45f840
begoIT24 <blopezprof@gmail.com>
2024-04-24 10:57:20 +0200

    setting user authentication

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,26 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/* Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+}); */
+
+Route::group([
+     'prefix' => 'dice_game'
+    ], function () {
+    Route::post('login', [AuthController::class, 'login']);  
+    Route::post('signup', [AuthController::class, 'signUp']);
+  
+    Route::group([
+      'middleware' => 'auth:api'
+    ], function() {
+        Route::get('logout', [AuthController::class, 'logout']);
+        Route::get('user', [AuthController::class, 'user']);
+    });
 });
+
+    
+
+
+
+
